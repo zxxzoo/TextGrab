dc48329e5fc64ca4065101e66471c15e79a141d3
zxxzoo <zzoog@outlook.com>
2026-09-25 14:07:26 +0800

    修复插件版本和Gradle版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 import java.util.Properties
 
 plugins {
-    id("com.android.application")
+    id("com.android.application") version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "1.9.24"
 }
 
 android {
